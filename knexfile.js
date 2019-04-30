b225994f172f0ea4b7e31a66555ca943ed6e7afa
Alando Appling <alandoappling@gmail.com>
2019-04-29 23:06:40 -0700

    Working on deploying to Herokku

--- knexfile.js
+++ knexfile.js
@@ -20,9 +20,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
